2cd3ad73eb6794cc87bb93d606aaef6efa0be2a9
lemonorangeapple <jcj1947725596@hotmail.com>
2024-01-28 16:47:53 +0800

    chore: sort the result

--- index.py
+++ index.py
@@ -7,4 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     res = json.loads(requests.get('http://api.github.com/repos/lemonorangeapple/edgeless-hub/releases', verify = False).text)
+    for release in res:
+        if release['prerelease'] == False:
+            res.remove(release)
+            res = [release] + res
+            break
     return render_template('index.html', res = res, markdown = markdown.markdown)
